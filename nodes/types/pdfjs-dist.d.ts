62bbf449911dbc4397da681c248a6f56c8ee4e07
BALZERTEC <patrick.balzer@balzertec.de>
2025-12-11 10:37:41 +0100

    Use supported pdfjs legacy entrypoint

---
+++ nodes/types/pdfjs-dist.d.ts
@@ -0,0 +1,5 @@
+declare module 'pdfjs-dist/legacy/build/pdf' {
+    export * from 'pdfjs-dist/types/src/pdf';
+    export { DocumentInitParameters } from 'pdfjs-dist/types/src/display/api';
+    export { stringToPDFString } from 'pdfjs-dist/types/src/shared/util';
+}
